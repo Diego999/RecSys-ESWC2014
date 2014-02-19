190a5c6528b43dfb067e5d5e5cec0584d2cbc51b
Diego Antognini <diegoantognini@gmail.com>
2014-02-19 23:07:33 +0100

    Change main

--- src/ch/hearc/p3/recsys/Main.java
+++ src/ch/hearc/p3/recsys/Main.java
@@ -1,10 +1,15 @@
 package ch.hearc.p3.recsys;
 
+import ch.hearc.p3.recsys.bookanalysis.Lemmatizer;
+import ch.hearc.p3.recsys.exception.KeyNotFoundException;
+
 public class Main
 {
 
 	public static void main(String[] args) throws KeyNotFoundException
 	{
-
+		String text = "Stanford University is located in California. It is a great university.";
+ 		for (String t : Lemmatizer.lemmatize(text))
+ 			System.out.println(t);
 	}
 }
