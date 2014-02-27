b8cde4861468dd2d7f0b2fe8c9346cddd235e0c3
Diego Antognini <diegoantognini@gmail.com>
2014-02-27 10:36:35 +0100

    Add matrix classes

---
+++ src/ch/hearc/p3/recsys/recommendation/Matrix1D.java
@@ -0,0 +1,41 @@
+package ch.hearc.p3.recsys.recommendation;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import ch.hearc.p3.recsys.exception.KeyNotFoundException;
+
+public class Matrix1D<K, V>
+{
+	private Map<K, V>	matrix;
+	private Set<K>		keys;
+
+	public Matrix1D(Set<K> keys, V defaultVal)
+	{
+		this.keys = new HashSet<K>(keys);
+		matrix = new HashMap<K, V>();
+		for (K key : keys)
+			matrix.put(key, defaultVal);
+	}
+
+	public V getItem(K key) throws KeyNotFoundException
+	{
+		if (!matrix.containsKey(key))
+			throw new KeyNotFoundException(key + " hasn't be found !");
+		return matrix.get(key);
+	}
+
+	public void setItem(K key, V value) throws KeyNotFoundException
+	{
+		if (!matrix.containsKey(key))
+			throw new KeyNotFoundException(key + " hasn't be found !");
+		matrix.put(key, value);
+	}
+
+	public Set<K> getCols()
+	{
+		return keys;
+	}
+}
---
+++ src/ch/hearc/p3/recsys/recommendation/Matrix2D.java
@@ -0,0 +1,57 @@
+package ch.hearc.p3.recsys.recommendation;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import ch.hearc.p3.recsys.exception.KeyNotFoundException;
+
+public class Matrix2D<K1, K2, V>
+{
+	private Map<K1, Map<K2, V>>	matrix;
+	private Set<K1>				keys1;
+	private Set<K2>				keys2;
+
+	public Matrix2D(Set<K1> keys1, Set<K2> keys2, V defaultValue)
+	{
+		this.keys1 = new HashSet<K1>(keys1);
+		this.keys2 = new HashSet<K2>(keys2);
+
+		matrix = new HashMap<K1, Map<K2, V>>();
+		for (K1 k1 : keys1)
+		{
+			matrix.put(k1, new HashMap<K2, V>());
+			for (K2 k2 : keys2)
+				matrix.get(k1).put(k2, defaultValue);
+		}
+	}
+
+	public V getItem(K1 k1, K2 k2) throws KeyNotFoundException
+	{
+		if (!matrix.containsKey(k1))
+			throw new KeyNotFoundException(k1 + " hasn't be found !");
+		if (!matrix.get(k1).containsKey(k2))
+			throw new KeyNotFoundException("(" + k1 + ", " + k2 + ") hasn't be found !");
+		return matrix.get(k1).get(k2);
+	}
+
+	public void setItem(K1 k1, K2 k2, V v) throws KeyNotFoundException
+	{
+		if (!matrix.containsKey(k1))
+			throw new KeyNotFoundException(k1 + " hasn't be found !");
+		if (!matrix.get(k1).containsKey(k2))
+			throw new KeyNotFoundException("(" + k1 + ", " + k2 + ") hasn't be found !");
+		matrix.get(k1).put(k2, v);
+	}
+
+	public Set<K1> getCols()
+	{
+		return keys1;
+	}
+
+	public Set<K2> getRows()
+	{
+		return keys2;
+	}
+}
